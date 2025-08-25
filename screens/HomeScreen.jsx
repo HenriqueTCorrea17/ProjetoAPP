2445d8d1932cfde68ab42de8f08f56d6e9ddd489
HenriqueTCorrea17 <henrique.t.correa@aluno.senai.br>
2025-08-25 11:30:12 -0300

    jjjj

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+import Header from "../components/Header";
+
+export {View} from "react-native";
+
+export default function HomeScreen () {
+    return (
+        <View>
+
+            <Header/>
+
+        </View>
+    )
+}
